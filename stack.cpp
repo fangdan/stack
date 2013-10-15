f884caf659e1ac35a1b5ca540df35a46e80864ef
fangdan <1027977224@qq.com>
2013-10-16 00:24:41 +0800

    linkstack

---
+++ stack.cpp
@@ -0,0 +1,51 @@
+//linkstack的简单实现
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+class Stack{
+private:
+class Node{
+public:  int data;
+		 Node *pre;
+		 Node(int a,Node *p):data(a),pre(p){};
+}*node;
+
+public: Stack():node(NULL){}
+		~Stack(){
+			while (node)
+			{
+				Node *p=node;
+				node=node->pre;
+				delete(p);
+			}
+		}
+		bool isempty(){
+			return node==NULL;
+		}
+		void putin(int elem){
+			node=new Node(elem,node);
+		}
+		int getout(){
+			int a=node->data;
+			Node *p=node;
+			node=node->pre;
+			delete p;
+			return a;
+		}
+};
+
+int main(){
+	Stack linkstack;
+	linkstack.putin(7);
+	linkstack.putin(10);
+	linkstack.putin(27);
+	while (!linkstack.isempty())
+	{
+		cout<<linkstack.getout()<<endl;
+	 };
+	system("pause");
+	return 0;
+
+};
